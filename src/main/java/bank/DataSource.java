d32ee63a6f3599df90dd63ac2b2a658520bbf354
HuyenNguyenT <118975956+HuyenNguyenT@users.noreply.github.com>
2022-12-02 03:02:57 +0000

    Create DataSource class

---
+++ src/main/java/bank/DataSource.java
@@ -0,0 +1,49 @@
+package bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+public class DataSource {
+  public static Connection connection(){
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+    try{
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("we're connection");
+    } catch(SQLException e){
+      e.printStackTrace();
+    }
+    return connection;
+  }
+
+  public static Customer getCustomer(String username){
+    String sql = "select * from customers where username =?";
+    Customer customer = null;
+    try(Connection connection = connection();
+    PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setString(1,username);
+      try(ResultSet resultSet = statement.executeQuery(sql)){
+        customer = new Customer(resultSet.getInt("id"),
+        resultSet.getString("name"),
+        resultSet.getString("username"),
+        resultSet.getString("password"),
+        resultSet.getInt("account_id"));
+      }
+    } catch(SQLException e){
+        e.printStackTrace();
+    }
+      return customer;
+  }
+
+  public static void main(String[] args){
+   
+    Customer customer = getCustomer("hnegal76@umn.edu");
+    System.out.println(customer.getName());
+  }
+}
+
