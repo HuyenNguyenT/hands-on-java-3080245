58c50e2d8a922aa472e6145fbff971c7bbb7ec62
HuyenNguyenT <118975956+HuyenNguyenT@users.noreply.github.com>
2022-12-04 15:10:48 +0000

    Add authenticated

---
+++ src/main/java/Authentication.java
@@ -0,0 +1,22 @@
+import javax.security.auth.login.LoginException;
+
+import bank.Customer;
+import bank.DataSource;
+
+public class Authentication {
+  public static Customer login(String username, String password) throws LoginException {
+    Customer customer = DataSource.getCustomer(username);
+    if(customer == null){
+      throw new LoginException("Username not found");
+    }
+    if(password.equals(customer.getPassword())){
+      customer.setAuthenticated(true);
+      return customer;
+    }
+    else throw new LoginException("Incorrect password")
+  }
+  public static void logout(Customer customer){
+    customer.setAuthenticated(false);
+
+  }
+}
